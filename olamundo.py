48806f246c4d8ad54d815722b3e7067833bcde8b
Aluno IFMT-PDL <aluno@ifmt.edu>
2023-09-26 17:25:12 -0400

    olamundo

--- olamundo.py
+++ olamundo.py
@@ -1,1 +1,9 @@
-print('ola mundo')
+par=0
+impar=0
+for i in range(1,2001):
+    if(i/2==0):
+        par=par+1
+    else:
+        impar=impar+1
+print(f'a quantidade de numeros pares é {par}')
+print(f'a quantidade de numeros ímpares é {impar}')
